673650e6d33756d9408e7240f2b34a012551a6e4
Joshua Rhoads <jarhoads@gmail.com>
2021-12-06 15:44:14 -0500

    Changes for Football Match Reports

---
+++ football-match-reports/FootballMatchReports.cs
@@ -0,0 +1,46 @@
+using System;
+
+public static class PlayAnalyzer
+{
+    public static string AnalyzeOnField(int shirtNum) => shirtNum switch
+	{
+        1 => "goalie",
+        2 => "left back",
+        3 => "center back",
+        4 => "center back",
+        5 => "right back",
+        6 => "midfielder",
+        7 => "midfielder",
+        8 => "midfielder",
+        9 => "left wing",
+        10 => "striker",
+        11 => "right wing",
+        _ => throw new ArgumentOutOfRangeException()
+	};
+
+    public static string AnalyzeOffField(object report)
+    {
+        if (report is int supporters)
+        {
+            return $"There are {supporters} supporters at the match.";
+        }
+        else if (report is string announcement)
+        {
+            return $"{announcement}";
+        }
+        else if (report is Incident incident)
+        {
+            return $"{incident.GetDescription()}";
+        }
+        else if (report is Manager manager)
+        {
+            return $"{manager}";
+        }
+        else
+        {
+            throw new ArgumentException();
+        }
+
+
+    }
+}
---
+++ football-match-reports/OffFieldActivitiesAndCharacters.cs
@@ -0,0 +1,36 @@
+﻿public class Manager
+{
+    public string Name { get; }
+
+    public string? Club { get; }
+
+    public Manager(string name, string? club)
+    {
+        this.Name = name;
+        this.Club = club ?? "";
+    }
+
+    public override string ToString() => string.IsNullOrWhiteSpace(Club) ? $"{Name}" : $"{Name} ({Club})";
+}
+
+public class Incident
+{
+    public virtual string GetDescription() => "An incident happened.";
+}
+
+public class Foul : Incident
+{
+    public override string GetDescription() => "The referee deemed a foul.";
+}
+
+public class Injury : Incident
+{
+    private readonly int player;
+
+    public Injury(int player)
+    {
+        this.player = player;
+    }
+
+    public override string GetDescription() => $"Oh no! Player {player} is injured. Medics are on the field.";
+}
